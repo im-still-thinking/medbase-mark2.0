20b7b09446f1daf9ad97c80473257898450473ee
Aroy <royaritra1990rocks@gmail.com>
2022-09-08 05:04:59 +0530

    phone authentication partly done

--- src/components/Authentication.jsx
+++ src/components/Authentication.jsx
@@ -8,6 +8,7 @@ export default function Authentication() {
 
   const [phoneNumber, setPhoneNumber] = useState(countryCode);
   const [expandForm, setExpandForm] = useState(false);
+  const [OTP, setOTP] = useState("");
 
   const generateReCaptcha = () => {
     window.recaptchaVerifier = new RecaptchaVerifier(
@@ -21,9 +22,14 @@ export default function Authentication() {
       );
   }
 
+  const handlePhoneNumber = (e) => {
+    console.log(e.target.value);
+    setPhoneNumber(e.target.value);
+  };
+
   const requestOTP = (e) => {
-    e.preventDefault();
-    if (phoneNumber.length >= 10) {
+    console.log(phoneNumber);
+    if (phoneNumber.length >= 12) {
       setExpandForm(true);
       generateReCaptcha();
       let appViewer = window.recaptchaVerifier;
@@ -34,44 +40,63 @@ export default function Authentication() {
         console.log(error);
       });
     }
+    e.preventDefault();
+  };
+
+  const verifyOTP = (e) => {
+    let otp = e.target.value;
+    setOTP(otp);
+
+    if(otp.length === 6){
+        let confirmationResult = window.confirmationResult;
+
+        confirmationResult.confirm(otp).then((result) => {
+            const user = result.user;
+            console.log(user);
+          }).catch((error) => {
+
+          });
+    }
   };
 
   return (
     <div className="container col-4">
       <div className="card mt-5">
+
         <div className="card-body">
           <form onSubmit={requestOTP}>
             <h3>Sign in</h3>
             <div className="mb-3">
-              <label htmlFor="phoneNumberInput" className="form-label">
-                Phone number
-              </label>
+              <label htmlFor="phoneNumberInput" className="form-label">Phone number</label>
               <input
+                onChange={handlePhoneNumber}
                 type="tel"
                 className="form-control"
                 id="phoneNumberInput"
                 aria-describedby="emailHelp"
+                value={phoneNumber}
               />
-              <div id="phoneNumberHelp" className="form-text">
-                Please enter your phone number
-              </div>
-            </div>
+              <div id="phoneNumberHelp" className="form-text">Please enter your phone number</div>
+            </div> 
+
             {expandForm === true ? (
               <>
                 <div className="mb-3">
                   <label htmlFor="otpInput" className="form-label">
                     OTP
                   </label>
-                  <input type="number" className="form-control" id="otpInput" />
+                  <input type="number" className="form-control" id="otpInput" val={OTP} onChange={verifyOTP} />
                   <div id="otpHelp" className="form-text">
                     Please enter the one time pin sent to your phone number
                   </div>
                 </div>
               </>
             ) : null}
+
             {expandForm === false ? (
-              <button className="btn btn-primary">Request OTP</button>
+              <button type="submit"className="btn btn-primary">Request OTP</button>
             ) : null}
+
             <div id="recaptcha-container"></div>
           </form>
         </div>
