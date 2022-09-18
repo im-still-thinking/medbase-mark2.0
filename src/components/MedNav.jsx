56b5d94e611e808f8174a00d402f16846bb758ae
Aroy <royaritra1990rocks@gmail.com>
2022-09-18 23:14:02 +0530

    updates

--- src/components/MedNav.jsx
+++ src/components/MedNav.jsx
@@ -1,16 +1,19 @@
-import Container from 'react-bootstrap/Container';
-import Navbar from 'react-bootstrap/Navbar';
+import Container from "react-bootstrap/Container";
+import Navbar from "react-bootstrap/Navbar";
+import { Link } from "react-router-dom";
 
 export default function MedNav() {
   return (
-    <Navbar bg="light" fixed="top" expand="lg" variant='light'>
+    <Navbar bg="light" fixed="top" expand="lg" variant="light">
       <Container fluid>
-        <Navbar.Brand href="#">MedBase</Navbar.Brand>
+        <Navbar.Brand href="/">MedBase</Navbar.Brand>
         <Navbar.Toggle aria-controls="navbarScroll" />
         <Navbar.Collapse className="justify-content-end">
-          <Navbar.Text>
-            Logout
-          </Navbar.Text>
+          <Link to="/auth">
+            <button className="btn btn-outline-success my-2 my-sm-0">
+              Log Out
+            </button>
+          </Link>
         </Navbar.Collapse>
       </Container>
     </Navbar>
