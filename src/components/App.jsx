ede8a79d327278825a73eddafdac02ff1aa7e061
SayakSarkar02 <sayaksarkar73@gmail.com>
2022-09-18 20:11:48 +0530

    css fix 1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,11 +3,13 @@ import { Route, Routes } from 'react-router-dom'
 import SignUp from './Auth/Signup';
 import Authentication from './Auth/Authentication'
 import Dashboard from './Dashboard';
+import Landing from './Landing';
 
 export default function App() {
   return (
     <Routes>
-      <Route path="/" element={<Authentication />} />
+      <Route path="/" element={<Landing />} />
+      <Route path="/auth" element={<Authentication />} />
       <Route path="/signup" element={<SignUp/>} />
       <Route path="/dashboard" element={<Dashboard />}/>
     </Routes>
---
+++ src/components/Landing.jsx
@@ -0,0 +1,79 @@
+import React from 'react'
+import sayak from '../../src/imgs/sayak.png';
+import risav from '../../src/imgs/risav.png';
+import raihan from '../../src/imgs/raihan.png';
+import aritra from '../../src/imgs/aritra.png';
+import one from '../../src/imgs/1.png';
+import three from '../../src/imgs/3.png';
+import thirteen from '../../src/imgs/13.png';
+import logo from '../../src/imgs/logo.png';
+import { Link } from 'react-router-dom';
+// if (window?.location.pathname === '/')
+//     require('../styless.css')
+import "../styless.css";
+
+
+
+export default function Landing() {
+  return (
+    <div className='body'>
+        <div className="navbar">
+        <center><img src={logo} alt="logo" className="navbarp"/></center>
+        </div>
+        <div className="shape1"></div>
+        <div className="shape2"></div>
+        <div className="shape3"></div>
+        <div className="shape4"></div>
+        <div className="shape5"></div>
+        <div className="shape6"></div>
+        <div className="land_bg">
+            <div className="card_land">
+                <h1 className='h1'><span className="span">Med</span>Base</h1>
+                <h3 className='h3'>Medical records <b className="any">ANYTIME</b>, <b className="any1">ANYWHERE!</b></h3>
+                <Link to="/auth"><button className="button">Join Us</button></Link>
+            </div>
+        </div>
+            <div className="about">
+                <h2 className="h2"><center>About Us</center></h2>
+                <p className="p fp">MedBase is an online platform to maintain medical records of a patient. It runs on the IPFS - InterPlanetary File System protocol making it accessible even from a different planet!
+                It's website form allows all users to use our services irrespective of the device they are using.
+                </p>
+                <br/>
+                <img src={one} alt="" className="first"/>
+            <p className="p sp">Every registered user gets a unique 12 digit generated ID which can be used for accessing all the medical records of the person from birth till date from anywhere across the globe. </p>
+            <br/>
+            <img src={three} alt="" className="second"/>
+            <p className="p tp">Once uploaded the document cannot be removed: This ensures that a document cannot be duplicated but demands utmost caution of the uploader before uploading any document. </p>
+            <img src={thirteen} alt="" className="third"/>
+                </div>
+    <div className="teams">
+        <h2 className="h2"><center>Our Team</center></h2>
+        <br/>
+        <br/>
+        <br/>
+        <div className="raihan">
+            <div className="border"><img src={raihan} alt=""/></div>
+            <h4>Raihan Khan</h4>
+            <h5 className="h5">Backend Developer</h5>
+        </div>
+        <div className="aritra">
+            <div className="border"><img src={aritra} alt=""/></div>
+            <h4>Aritra Roy</h4>
+            <h5 className="h5">Backend Developer</h5>
+        </div>
+        <div className="risav">
+            <div className="border"><img src={risav} alt="Risav"/></div>
+            <h4>Risavdeb Patra</h4>
+            <h5 className="h5">Frontend Developer</h5></div>
+        <div className="sayak">
+            <div className="border"><img src={sayak} alt="Sayak"/></div>
+            <h4>Sayak Sarkar</h4>
+            <h5 className="h5">Frontend Developer</h5>
+        </div>
+    </div>
+    <div className="bottom_land">
+        <h5 className="h5">©Zephyrus</h5>
+    </div>
+    </div>
+  )
+}
