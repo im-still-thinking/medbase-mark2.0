770cbc887a52df4774f4cb3be94d09b71fbcc11d
SayakSarkar02 <sayaksarkar73@gmail.com>
2022-10-15 23:35:53 +0530

    fix

--- src/components/MedNav.jsx
+++ src/components/MedNav.jsx
@@ -1,5 +1,5 @@
-import Container from "react-bootstrap/Container";
-import Navbar from "react-bootstrap/Navbar";
+//import Container from "react-bootstrap/Container";
+//import Navbar from "react-bootstrap/Navbar";
 import { Link } from "react-router-dom";
 import logo from "../../src/imgs/MedBase.png";
 
