50311574ac09c5cf1528151155e4395f2481e4be
SayakSarkar02 <sayaksarkar73@gmail.com>
2023-11-23 22:18:37 +0530

    push

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -2,15 +2,7 @@ import { initializeApp } from "firebase/app";
 import {getAuth} from 'firebase/auth'
 import { getFirestore } from "firebase/firestore";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyCbQPZHKpD_1SqyxyH-vXkFewH2Dp4xiMA",
-  authDomain: "medbaserevamp.firebaseapp.com",
-  projectId: "medbaserevamp",
-  storageBucket: "medbaserevamp.appspot.com",
-  messagingSenderId: "979990689981",
-  appId: "1:979990689981:web:761c43f384acd713fd9fd4",
-  measurementId: "G-VRMVQ12XXQ"
-};
+
 
 
 const app = initializeApp(firebaseConfig);
